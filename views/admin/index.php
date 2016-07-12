c96e7edf208548a6d5cd419eda988b23a1fdcfc3
Manuel Kuhlmann <manuel@mkuhlmann.org>
2016-07-12 18:03:54 +0200

    Improve slug edit

--- views/admin/index.php
+++ views/admin/index.php
@@ -23,10 +23,13 @@
 						<a href="{{ item.path }}">{{ item.title }}<span v-if="item.type == 'url'">[...]</span></a>
 					</span>
 				</td>
-				<td>
-					<span class="slug"><?php echo app()->config->baseUrl; ?>/<input type="text" v-on:keyup="itemSlugKeyPress(item, $event)" v-model="item.slug"></span>
-					 <i v-show="item.modified && item.slug.length > 0" v-on:click="itemSlugSave(item)" class="save icon opacity-hover pointer"></i>
-					 <i v-show="item.modified" v-on:click="itemSlugCancel(item)" class="cancel icon opacity-hover pointer"></i>
+				<td class="slug">
+					<span v-on:click="itemSlugEdit(item)" v-show="item.modified == false"><?php echo app()->config->baseUrl; ?>/{{ item.slug }}</span>
+					<span v-show="item.modified == true">
+						<?php echo app()->config->baseUrl; ?>/<input type="text" v-bind:id="'ise' + item.id" v-on:keyup="itemSlugKeyPress(item, $event)" v-model="item.slug">
+					 	<i v-show="item.slug.length > 0" v-on:click="itemSlugSave(item)" class="save icon opacity-hover pointer"></i>
+					 	<i v-on:click="itemSlugCancel(item)" class="cancel icon opacity-hover pointer"></i>
+					</span>
 				 </td>
 				<td>{{ item.clicks || 0 }}</td>
 				<td>{{ item.type }}</td>
@@ -46,10 +49,11 @@ app.IndexCtrl = Vue.extend({
 	} },
 	created: function() {
 		this.$http.get('/api/v1/items').then(function(response) {
-			this.items = response.data;
-			for(var i = 0; i < this.items.length; i++) {
-				this.items[i].oldSlug = this.items[i].slug;
+		 	var items = response.data;
+			for(var i = 0; i < items.length; i++) {
+				items[i].modified = false;
 			}
+			this.items = items;
 		});
 	},
 	methods: {
@@ -60,10 +64,10 @@ app.IndexCtrl = Vue.extend({
 			this.imageThumbPath = null;
 		},
 		itemDelete: function(item) {
+			item.title = 'wird gelöscht ...';
 			this.$http.post('/api/v1/item/'+item.slug+'/delete', {'_csrf': app._csrf}).then(function(repsonse) {
-
+				this.items.splice(this.items.indexOf(item), 1);
 			});
-			this.items.splice(this.items.indexOf(item), 1);
 		},
 		itemSlugKeyPress: function(item, e) {
 			if(!item.modified) {
@@ -75,6 +79,13 @@ app.IndexCtrl = Vue.extend({
 				return false;
 			}
 		},
+		itemSlugEdit: function(item) {
+			item.modified = true;
+			item.oldSlug = item.slug;
+			window.setTimeout(function() {
+				document.getElementById('ise'+item.id).focus();
+			}, 50);
+		},
 		itemSlugCancel: function(item) {
 			item.slug = item.oldSlug;
 			item.modified = false;
@@ -85,7 +96,7 @@ app.IndexCtrl = Vue.extend({
 			item.slug = 'speichere ...';
 
 			this.$http.post('/api/v1/item/'+item.oldSlug+'/update', {'_csrf': app._csrf, 'item': sendItem }).then(function(response) {
-				this.$set('items['+this.items.indexOf(item)+']', Object.assign(response.data, {oldSlug: response.data.slug}));
+				this.$set('items['+this.items.indexOf(item)+']', Object.assign(response.data, {modified: false}));
 			});
 		}
 	}
