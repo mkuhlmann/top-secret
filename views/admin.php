ac2847571f88b8d00029114a66ceaec3e70113a1
Manuel Kuhlmann <manuel@mkuhlmann.org>
2016-07-28 18:16:44 +0200

    Style changes

--- views/admin.php
+++ views/admin.php
@@ -5,7 +5,7 @@
 	<style type="text/css">
 	body { top: 0; left: 0; }
 	a { cursor: pointer; }
-	#app { padding: 40px; }
+	#app { padding: 10px 20px; }
 	.pointer {
 		cursor: pointer;
 	}
@@ -36,69 +36,61 @@
 				<div class="ui text large loader"><?php echo app()->config->pageName; ?> wird geladen ...</div>
 			</div>
 		</div>
-		<div class="ui top attached menu">
-			<a class="ui item" href="/tsa/logout">
-				<i class="sign out icon"></i> Logout
+		<div class="ui secondary pointing menu">
+			<a class="item" v-on:click="switchMenu(null)" v-bind:class="{ active: menu == null }">
+				<i class="list icon"></i> Start
 			</a>
-			<a class="ui item" v-on:click="switchMenu('settings')">
+			<a class="item" v-on:click="switchMenu('settings')" v-bind:class="{ active: menu == 'settings' }">
 				<i class="wrench icon"></i> Einstellungen
 			</a>
-			<a class="ui item" v-on:click="switchMenu('tags')">
+			<a class="item" v-on:click="switchMenu('tags')" v-bind:class="{ active: menu == 'tags' }">
 				<i class="flag icon"></i> Tags
 			</a>
-			<a class="ui item" v-on:click="switchMenu('howto')">
+			<a class="item" v-on:click="switchMenu('howto')" v-bind:class="{ active: menu == 'howto' }">
 				<i class="help circle icon"></i> Anleitung
 			</a>
 			<div class="right menu">
-				<div class="ui right aligned category search item">
-					<div class="ui transparent icon input">
-						<input class="prompt" type="text" placeholder="Suche ...">
-						<i class="search link icon"></i>
-					</div>
-					<div class="results"></div>
-				</div>
+				<a class="item" href="/tsa/logout">
+					<i class="sign out icon"></i> Logout
+				</a>
 			</div>
 		</div>
-		<div class="ui bottom attached segment hidden" v-show="menu != null">
-			<div v-if="menu == 'settings'"><settings-ctrl></settings-ctrl></div>
-			<div v-if="menu == 'tags'"><tags-ctrl></tags-ctrl></div>
-			<div class="ui container" v-if="menu == 'howto'">
-				<div class="ui grid">
-					<div class="six wide column">
-						<h2>ShareX</h2>
-						<div class="ui form">
-							<div class="field">
-								<label>Hochlade URL</label>
-								<input type="text" value="<?php echo app()->config->baseUrl . '/api/v1/upload?key='.app()->config->apiKey; ?>">
-							</div>
-							<div class="field">
-								<label>JSON Ergebnis</label>
-								<input type="text" value="<?php echo app()->config->baseUrl . '/$json:slug$.$json.extension$'; ?>">
-							</div>
+		<div v-if="menu == 'settings'"><settings-ctrl></settings-ctrl></div>
+		<div v-if="menu == 'tags'"><tags-ctrl></tags-ctrl></div>
+		<div class="ui container" v-if="menu == 'howto'">
+			<div class="ui grid">
+				<div class="six wide column">
+					<h2>ShareX</h2>
+					<div class="ui form">
+						<div class="field">
+							<label>Hochlade URL</label>
+							<input type="text" value="<?php echo app()->config->baseUrl . '/api/v1/upload?key='.app()->config->apiKey; ?>">
 						</div>
+						<div class="field">
+							<label>JSON Ergebnis</label>
+							<input type="text" value="<?php echo app()->config->baseUrl . '/$json:slug$.$json.extension$'; ?>">
+						</div>
+					</div>
 
-						<img v-bind:src="'https://top-secret.xyz/j2oMS6.png'" />
-					</div>
-					<div class="six wide column">
-						<h2>Tasker</h2>
-						<p>
-							<ul>
-								<li><a href="https://play.google.com/store/apps/details?id=com.joaomgcd.autoshare" target="_blank" rel="nofollow">AutoShare</a> installieren und neues AutoShare-Command namens "top-secret" hinzufügen.</li>
-								<li>Lade das <a href="/tsa/Tasker.prf.xml">Tasker Profil</a> herunter.</li>
-								<li>In Tasker importieren</li>
-								<li>Die Pushbullet Aufgabe neu konfigurieren.</li>
-								<li>???</li>
-								<li>Profit.</li>
-							</ul>
-						</p>
-					</div>
+					<img v-bind:src="'https://top-secret.xyz/j2oMS6.png'">
 				</div>
-
+				<div class="six wide column">
+					<h2>Tasker</h2>
+					<p>
+						<ul>
+							<li><a href="https://play.google.com/store/apps/details?id=com.joaomgcd.autoshare" target="_blank" rel="nofollow">AutoShare</a> installieren und neues AutoShare-Command namens "top-secret" hinzufügen.</li>
+							<li>Lade das <a href="/tsa/Tasker.prf.xml">Tasker Profil</a> herunter.</li>
+							<li>In Tasker importieren</li>
+							<li>Die Pushbullet Aufgabe neu konfigurieren.</li>
+							<li>???</li>
+							<li>Profit.</li>
+						</ul>
+					</p>
+				</div>
 			</div>
+
 		</div>
-		<div v-if="menu != 'tags'">
-			<index-ctrl></index-ctrl>
-		</div>
+		<div v-if="menu == null"><index-ctrl></index-ctrl></div>
 	</div>
 
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
@@ -120,7 +112,7 @@
 			created: function() {},
 			methods: {
 				switchMenu: function(key) {
-					this.menu = (this.menu == key) ? null : key;
+					this.menu = key;
 				}
 			}
 		});
@@ -128,7 +120,7 @@
 		window.onload = function() {
 			new app.Root({ el: '#app' });
 			$('.ui.dropdown.onload').dropdown();
-		}
+		};
 	</script>
 	<!-- vue.js templates here -->
 	<?php $files = glob(dirname(__FILE__).'/admin/*.{html,php}', GLOB_BRACE);
