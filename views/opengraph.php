e6b76092c189ba51cbfcf373b57dcfee7516aa0c
Manuel Kuhlmann <manuel@mkuhlmann.org>
2018-09-12 12:13:31 +0200

    Really need to clean this mess up

--- views/opengraph.php
+++ views/opengraph.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta property="og:image" content="<?php echo app()->config->baseUrl . '/thumb/' . $item->slug ?>" />
-		<meta property="og:image:width" content="<?php echo $thumbSize[0]; ?>" />
-		<meta property="og:image:height" content="<?php echo $thumbSize[1]; ?>" />
+		<meta property="og:image" content="<?php echo app()->config->baseUrl . '/thumb/' . $item->slug ?>.jpg" />
+		<meta property="og:image:width" content="<?php echo round($thumbSize[0]); ?>" />
+		<meta property="og:image:height" content="<?php echo round($thumbSize[1]); ?>" />
 		<meta property="og:description" content="<?php echo e(app()->config->pageName); ?>" />
 		<meta property="og:title" content="<?php echo e($item->title); ?>" />
 		<title><?php echo e($item->title); ?></title>
