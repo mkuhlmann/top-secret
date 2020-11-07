4030a8c55e67a368cfe2231be0292d03322c9167
Manuel Kuhlmann <manuel@mkuhlmann.org>
2020-11-07 16:39:16 +0000

    Bump few dependencies

--- views/admin2.php
+++ views/admin2.php
@@ -5,8 +5,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		
-		<link href="https://unpkg.com/buefy@0.8.13/dist/buefy.min.css" rel="stylesheet" integrity="sha256-wCIznvmMv8MeshDLmooVTAYXRcX77UMaIp8gyyYMPQ8=" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.5.95/css/materialdesignicons.min.css" integrity="sha384-K4kKEbDs5+0KuqneFn9bbf36Gbp70oNEX6HB+IwiTCGJaGBTQyjYxFL9Z/ZQcoY5" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/buefy/0.9.4/buefy.min.css" integrity="sha512-kYGHZRStwK4F8bgVhj/J5IEWmEjLbQ7re6mQiYx/LH5pfl8bDQ3g5SaExM/6z59mASfENR8xwVhywnm8ulVvew==" crossorigin="anonymous" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/5.8.55/css/materialdesignicons.min.css" integrity="sha512-x96qcyADhiw/CZY7QLOo7dB8i/REOEHZDhNfoDuJlyQ+yZzhdy91eAa4EkO7g3egt8obvLeJPoUKEKu5C5JYjA==" crossorigin="anonymous" />
 		
 		<meta name="key" content="%API_KEY%">
 		<meta name="csrf" content="<?php echo app()->session->token(); ?>">
@@ -28,8 +28,8 @@
 		<?php endif; ?>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-i18n/8.17.4/vue-i18n.min.js" integrity="sha256-nNm7R+HTeuAIJA3rTufGjoe3r6y6iIYEu4QWm1y+yNY=" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.1.3/vue-router.min.js" integrity="sha256-r/vPIUvTobCpYZsMmvU7HM58cNd4D6/vdICqDFapV7Y=" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js" integrity="sha256-4iQZ6BVL4qNKlQ27TExEhBN1HFPvAvAMbFavKKosSWQ=" crossorigin="anonymous"></script>
-		<script src="https://unpkg.com/buefy@0.8.13/dist/buefy.min.js" integrity="sha256-4b3psIbMGiTsSFoLwsUdnfJ+NxNf6C/wLsXgzRQ87d8=" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/buefy/0.9.4/buefy.min.js" integrity="sha512-ejJ4Nw/I2BSgEeJoP85Joh0nfMyXT+US/UlnDmp7GC20yrUDhoerj7RnOGOQK0Ke3vPhYJy0eicEvCaVldktXA==" crossorigin="anonymous"></script>
 		<script type="module" src="/App/Index.js"></script>
 	</body>
 	
